b059e7a7a89cd54c4c84dc42425ef2e1359109ac
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-23 00:38:29 +0400

    define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
 function App() {
-  return <>ahmad</>;
+  return (
+    <RouterProvider
+      router={createBrowserRouter(
+        createRoutesFromElements(
+          <>
+            <Route path='/' element={<></>}>
+              <Route path='users' element={<p>users</p>} />
+              <Route path='product' element={<p>products</p>} />
+            </Route>
+            <Route path='*' element={<p>error</p>} />
+          </>,
+        ),
+      )}
+    />
+  );
 }
 
 export default App;
