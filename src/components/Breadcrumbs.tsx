49fd60f286aa6208a49ebc26eb286b18ceaa57a4
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-23 01:29:57 +0400

    finishing breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+
 export default function Breadcrumbs() {
   const location = useLocation();
   const pathnames = location.pathname.split('/').filter((path) => path !== '');
+
   return (
-    <nav aria-label='breadcrumb'>
-      <ol className='breadcrumb'>
-        <li className='breadcrumb-item'>
-          <Link to='/'>Home</Link>
+    <nav>
+      <ol className='m-0 p-0 flex flex-wrap'>
+        <li>
+          <Link to='/' className='pr-1'>
+            Home
+          </Link>
         </li>
         {pathnames.map((path, index) => {
           const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isLast = index === pathnames.length - 1;
 
           return (
-            <li key={index} className={`breadcrumb-item ${isLast ? 'active' : ''}`}>
-              {isLast ? path : <Link to={routeTo}>{path}</Link>}
+            <li key={index}>
+              /
+              <Link
+                to={routeTo}
+                className={`${isLast ? 'halfColor w-fit fo font-bold ml-2' : 'pr-1'}`}
+              >
+                {path.charAt(0).toUpperCase() + path.slice(1)}
+              </Link>
             </li>
           );
         })}
