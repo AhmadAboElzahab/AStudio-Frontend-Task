dc9e2d5d0571ab064cf2d0e1270ab4346e0026be
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-23 00:29:59 +0400

    define default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#322625',
+        grey: '#ebebeb',
+        blue: '#c0e3e5',
+        yellow: '#fdc936',
+      },
+    },
   },
   plugins: [],
 };
