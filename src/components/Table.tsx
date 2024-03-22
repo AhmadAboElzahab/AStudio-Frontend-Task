2b02dd0c8c0d5b32d749d4f4970ca5611e419982
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-23 02:46:07 +0400

    initial render

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,23 +3,30 @@ import React from 'react';
 type TableProps = {
   headers: string[];
   data: unknown[];
-  renderRow: (item: unknown, index: number) => React.ReactNode;
+  renderRow: (item: any, index: number) => React.ReactNode;
 };
 
 export default function Table({ headers, data, renderRow }: TableProps) {
   return (
     <div>
-      <table>
+      <table className='w-full text-left '>
         <thead>
           <tr>
             {headers.map((header, index) => (
-              <th key={index}>{header}</th>
+              <th
+                key={index}
+                className='w-fit border px-[20px] py-3 border-gray bg-blue uppercase   text-nowrap font-semibold '
+              >
+                {header}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
           {data.map((item, index) => (
-            <tr key={index}>{renderRow(item, index)}</tr>
+            <tr key={index} className='hover:bg-grey'>
+              {renderRow(item, index)}
+            </tr>
           ))}
         </tbody>
       </table>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { UserData, User } from '../types/type';
+import customAxios from '../lib/axios';
+import Table from '../components/Table';
 
 export default function Users() {
+  const [users, setUsers] = useState<UserData>({ users: [], total: 0, skip: 0, limit: 0 });
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await customAxios.get('/users');
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const renderUserRow = (user: User, index: number) => {
+    return (
+      <>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.maidenName}</td>
+        <td>{user.age}</td>
+        <td>{user.gender}</td>
+        <td>{user.email}</td>
+        <td>{user.username}</td>
+        <td>{user.bloodGroup}</td>
+        <td>{user.eyeColor}</td>
+      </>
+    );
+  };
+
   return (
-    <div>Users</div>
-  )
+    <div>
+      <Table
+        headers={[
+          'First Name',
+          'Last Name',
+          'Maiden Name',
+          'Age',
+          'Gender',
+          'Email',
+          'Username',
+          'Bloodgroup',
+          'EyeColor',
+        ]}
+        data={users.users}
+        renderRow={renderUserRow}
+      />
+    </div>
+  );
 }
