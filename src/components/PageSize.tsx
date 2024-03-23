4da4f255e2e431aac55a7c9bce9a1967fec15f01
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-24 03:04:59 +0400

    edit values

--- src/components/PageSize.tsx
+++ src/components/PageSize.tsx
@@ -17,7 +17,7 @@ const PageSize: React.FC<PageSizeProps> = ({ onPageSizeChange, data }) => {
         <option value='5'>5</option>
         <option value='10'>10</option>
         <option value='20'>20</option>
-        <option value='30'>30</option>
+        <option value='50'>50</option>
       </select>
     </div>
   );
