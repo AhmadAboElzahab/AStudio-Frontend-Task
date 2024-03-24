98793ea59b3eff636e7b2c72d3f3d23150afda46
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-24 04:06:50 +0400

    add home links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,14 @@
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='text-4xl flex flex-col justify-center items-center h-60  '>
+      <Link to='/users' className='hover:bg-yellow p-5 rounded'>
+        Users
+      </Link>
+      <Link to='/products' className='hover:bg-yellow p-5 rounded'>
+        Products
+      </Link>
+    </div>
+  );
 }
